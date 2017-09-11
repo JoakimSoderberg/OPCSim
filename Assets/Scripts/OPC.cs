cf146e26df900a102d3415e72142fe5308d5dfd0
Joakim Soderberg <joakim.soderberg@netrounds.com>
2017-09-12 01:05:35 +0200

    Add OPC server

--- Assets/Scripts/OPC.cs
+++ Assets/Scripts/OPC.cs
@@ -4,6 +4,37 @@ using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
+using System;
+
+struct Pixel
+{
+    public byte r;
+    public byte g;
+    public byte b;
+
+    public Pixel(byte r, byte g, byte b)
+    {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
+
+public class OPCClient
+{
+    public const int HDR_SIZE = 4;
+    public byte[] payload = new byte[1 << 16];
+    public byte[] hdr = new byte[HDR_SIZE];
+    public byte command;
+    public byte channel;
+    public int payload_expected;
+    public TcpClient client;
+
+    public OPCClient(TcpClient client)
+    {
+        this.client = client;
+    }
+}
 
 public class OPC : MonoBehaviour
 {
@@ -11,44 +42,84 @@ public class OPC : MonoBehaviour
     private Thread mThread;
     private TcpListener listener = null;
 
+    // OPC broadcast channel.
+    const int OPC_BROADCAST = 0;
+
+    // Command codes.
+    const int OPC_SET_PIXELS = 0;
+
+    // Max number of OPC sinks or sources allowed.
+    const int OPC_MAX_SINKS = 64;
+    const int OPC_MAX_SORUCES = 64;
+
+    // Max number of pixels per message.
+    const int OPC_MAX_PIXELS_PER_MESSAGE = ((1 << 16) / 3);
+
+
     void Start()
-    {
-        mRunning = true;
-        ThreadStart ts = new ThreadStart(Receive);
-        mThread = new Thread(ts);
-        mThread.Start();
-        Debug.Log("Started OCP Thread...");
-    }
-
-    public void stopListening()
-    {
-        mRunning = false;
-    }
-
-    void Receive()
     {
         listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7890);
         listener.Start();
-        Debug.Log("Server start");
+        listener.BeginAcceptTcpClient(OPCHandler, null);
+        Debug.Log("OPC: Server started listening for clients...");
+    }
 
-        while (mRunning)
+    private void OPCHandler(IAsyncResult accept_result)
+    {
+        TcpClient client = listener.EndAcceptTcpClient(accept_result);
+
+        var opclient = new OPCClient(client);
+
+        // Listen again.
+        listener.BeginAcceptTcpClient(OPCHandler, null);
+
+        Debug.Log("OPC: Client connected from " + client.Client.RemoteEndPoint.ToString());
+        var stream = client.GetStream();
+
+        stream.BeginRead(opclient.hdr, 0, OPCClient.HDR_SIZE, new AsyncCallback(ReadOPCHeader), opclient);
+    }
+
+    private void ReadOPCPayload(IAsyncResult payload_result)
+    {
+        var opclient = payload_result.AsyncState as OPCClient;
+        NetworkStream stream = opclient.client.GetStream();
+        int payload_len = stream.EndRead(payload_result);
+
+        if (payload_len == 0)
         {
-            // check if new connections are pending, if not, be nice and sleep 100ms
-            if (!listener.Pending())
+            Debug.Log("Disconnected ");
+        }
+        else
+        {
+            Debug.Log("Read " + payload_len);
+
+            if (opclient.command == OPC_SET_PIXELS)
             {
-                Thread.Sleep(100);
-            }
-            else
-            {
-                TcpClient client = listener.AcceptTcpClient();
-                Debug.Log("OPC: Client connected from " + client.Client.RemoteEndPoint.ToString());
-                /*Socket ss = tcp_Listener.AcceptSocket();
-                Debug.Log("OPC: Client connected from " + ss.a);
-                byte[] tempbuffer = new byte[10000];
-                ss.Receive(tempbuffer); // received byte array from client
-                */
+                //PixelHandler(opclient.channel, (ushort)(payload_len / 3), opclient.payload);
             }
         }
+
+        stream.BeginRead(opclient.hdr, 0, OPCClient.HDR_SIZE, new AsyncCallback(ReadOPCHeader), opclient);
+    }
+
+    private void ReadOPCHeader(IAsyncResult hdr_result)
+    {
+        var opclient = hdr_result.AsyncState as OPCClient;
+        NetworkStream stream = opclient.client.GetStream();
+
+        int hdr_len = stream.EndRead(hdr_result);
+        opclient.channel = (byte)opclient.hdr[0];
+        opclient.command = (byte)opclient.hdr[1];
+        opclient.payload_expected = (((byte)opclient.hdr[2] << 8) | (byte)opclient.hdr[3]);
+        Debug.Log("Packet ch " + opclient.channel + " cmd " + opclient.command + " => " + opclient.payload_expected);
+
+        // Listen for next packet.
+        if (opclient.payload_expected < opclient.payload.Length)
+        {
+            opclient.payload = new byte[opclient.payload_expected];
+        }
+
+        stream.BeginRead(opclient.payload, 0, opclient.payload_expected, new AsyncCallback(ReadOPCPayload), opclient);
     }
 
     void Update()
@@ -56,10 +127,12 @@ public class OPC : MonoBehaviour
 
     }
 
-    void OnApplicationQuit()
+    void PixelHandler(ushort channel, ushort count, byte[] data)
     {
-        // stop listening thread
-        stopListening(); // wait for listening thread to terminate (max. 500ms)
-        mThread.Join(500);
+        for (ushort i = 0; i < count; i++)
+        {
+            Pixel p = new Pixel(data[i], data[i + 1], data[i + 2]);
+            Debug.Log(p.r + " " + p.g + " " + p.b);
+        }
     }
 }
